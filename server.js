b62e8b491c30b4a7c275dbce7e1589b26c1f44b5
Muhammad Kazim <kazim@192.168.1.7>
2024-05-26 03:45:22 +0500

    what: add crud operations for products impact: medium

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const productRoutes = require("./routes/productRoutes");
+const errorHandler = require("./middleware/errorHandler");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// app.get("/api/contacts", (req, res) => {
+//   res.status(200).json({message: "all clients"});
+// });
+app.use(express.json());
+app.use("/api/products", productRoutes);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
